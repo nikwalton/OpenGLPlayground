78f0f40ab19fec84947b49e5a4e0d90d36e582ff
Nikolaus Walton <nikawalton2@gmail.com>
2021-06-18 23:53:20 -0700

    Initial Commit

---
+++ OpenGLPlayground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLPlayground
+//
+//  Created by Nikolaus Walton on 6/18/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
