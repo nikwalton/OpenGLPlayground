35abd459d65eec2f9aec71bbb667ce514a6dad42
Nikolaus Walton <nikawalton2@gmail.com>
2021-06-20 16:12:43 -0700

    Finished with the shaders for the triangle

--- OpenGLPlayground/main.cpp
+++ OpenGLPlayground/main.cpp
@@ -17,13 +17,20 @@ const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
 
 
-//temp storing glsl shader for now
+//temp storing glsl shaders for now
 const char *vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main()\n"
 	"{\n"
 	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 	"}\n";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
 
 int main() {
 	// initalize and configure glfw
@@ -53,19 +60,43 @@ int main() {
 		return -1;
 	}
 	
-	//shader program
+	//MARK: shader program
 	//vertex shader
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-	// shader error checking
+	// vertex shader error checking
 	int success;
 	char log[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
-		glGetShaderInfoLog(vertexShader, 521, NULL, log);
+		glGetShaderInfoLog(vertexShader, 512, NULL, log);
 		std::cout << "Error shader: vertex compilation failed!\n" << log << std::endl;
 	}
+	//frag shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// frag shader checking
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, log);
+		std::cout << "Error shader: fragment compilation failed\n!" << log << std::endl;
+	}
+	
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, log);
+		std::cout << "Error Shader Program: Error linking shaders!\n" << log << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	//define triangle
 	float verticies[] = {
