dcff80a6d9a9aef55c9b1d61e4a7475801b14c4e
Nikolaus Walton <nikawalton2@gmail.com>
2021-06-20 17:41:46 -0700

    Finished section on drawing the basic triangle, made a rectangle

--- OpenGLPlayground/main.cpp
+++ OpenGLPlayground/main.cpp
@@ -99,30 +99,61 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 	//define triangle
+	// can make a rectangle by making a second triangle
 	float verticies[] = {
-		-0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
 	};
 	
-	unsigned int VBO;
+	//EBO allows us to only store unique verticies and then specify the order
+	unsigned int indices[] = { //start at 0 as usual
+		0, 1, 3, //first triangle
+		1, 2, 3 //second triangle
+	};
+	
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 	
 	//MARK: Render Loop!
 	while (!glfwWindowShouldClose(window)) {
 		//input
 		processInput(window);
 		
-		glClearColor(0.2f, 0.3f, 0.3, 0.1f);
+		glClearColor(0.2f, 0.3f, 0.3, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		
 	}
 	
-
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 	
 	glfwTerminate();
 	return 0;
